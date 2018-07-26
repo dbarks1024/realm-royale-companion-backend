fd760e79063621ce8e68e785b5fb24b367284e45
dbarks1024 <dbarker@bignerdranch.com>
2018-07-25 22:46:16 -0400

    refactor signature creation

--- helper/helper.js
+++ helper/helper.js
@@ -1,7 +1,12 @@
 const moment = require ('moment');
+const md5 = require('md5');
 
 const currentTime = () => {
   return moment.utc().format('YYYYMMDDHHmmss');
 };
 
-module.exports = { currentTime };
+const createSignature = (method) => {
+  return md5(`${process.env.DEV_ID}${method}${process.env.AUTH_KEY}${currentTime()}`);
+};
+
+module.exports = { currentTime, createSignature };
--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,12 @@ const express = require('express');
 const router = express.Router();
 const rp = require('request-promise');
 const md5 = require('md5');
-const { currentTime } = require('../helper/helper');
+const { currentTime, createSignature } = require('../helper/helper');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   const method = 'createsession';
-  const timeStamp = currentTime();
-  const devId = process.env.DEV_ID;
-  const authKey = process.env.AUTH_KEY;
-  console.log(currentTime());
-  const signature = md5(`${devId}${method}${authKey}${timeStamp}`);
-  console.log(process.env.AUTH_KEY);
-  const url =  `http://api.realmroyale.com/realmapi.svc/${method}JSON/${process.env.DEV_ID}/${signature}/${timeStamp}`;
+  const url =  `http://api.realmroyale.com/realmapi.svc/${method}JSON/${process.env.DEV_ID}/${createSignature(method)}/${currentTime()}`;
   
   rp(url)
   .then((response) => {
