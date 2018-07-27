639465d3498c0b56997c5e097b1f2cb7eda8f7a8
dbarks1024 <dbarker@bignerdranch.com>
2018-07-27 08:38:31 -0400

    Add promise to create session

--- helper/helper.js
+++ helper/helper.js
@@ -1,5 +1,6 @@
-const moment = require ('moment');
+const moment = require('moment');
 const md5 = require('md5');
+const rp = require('request-promise');
 
 const currentTime = () => {
   return moment.utc().format('YYYYMMDDHHmmss');
@@ -8,5 +9,26 @@ const currentTime = () => {
 const createSignature = (method) => {
   return md5(`${process.env.DEV_ID}${method}${process.env.AUTH_KEY}${currentTime()}`);
 };
+const createSession = () => {
+  const method = 'createsession';
+  const url = `http://api.realmroyale.com/realmapi.svc/${method}JSON/${process.env.DEV_ID}/${createSignature(method)}/${currentTime()}`;
 
-module.exports = { currentTime, createSignature };
+  return new Promise((resolve, reject) => {
+    rp(url)
+      .then((response) => {
+        console.log(response);
+        resolve(JSON.parse(response));
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
+};
+
+
+module.exports = {
+  currentTime,
+  createSignature,
+  createSession
+};
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const rp = require('request-promise');
-const md5 = require('md5');
-const { currentTime, createSignature } = require('../helper/helper');
+
+const { createSession } = require('../helper/helper');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  const method = 'createsession';
-  const url =  `http://api.realmroyale.com/realmapi.svc/${method}JSON/${process.env.DEV_ID}/${createSignature(method)}/${currentTime()}`;
+  const method = 'ping';
+  const url =  `http://api.realmroyale.com/realmapi.svc/${method}JSON`;
   
-  rp(url)
-  .then((response) => {
-    return JSON.parse(response);
-  })
-  .then((response) => {
+  createSession()
+  .then((sesresponsesion) => {
     res.json(response);
-  })
-  .catch((error) => {
-    console.log(error);
   });
-
 });
 
 module.exports = router;
