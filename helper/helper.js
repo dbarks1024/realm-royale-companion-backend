602b1e687658f4f3fb966afb2ba1c8512b135afb
dbarks1024 <dbarker@bignerdranch.com>
2018-07-27 13:37:06 -0400

    Add leaderboard post route

--- helper/helper.js
+++ helper/helper.js
@@ -9,6 +9,8 @@ const currentTime = () => {
 const createSignature = (method) => {
   return md5(`${process.env.DEV_ID}${method}${process.env.AUTH_KEY}${currentTime()}`);
 };
+
+//put these in a class?
 const createSession = () => {
   const method = 'createsession';
   const url = `http://api.realmroyale.com/realmapi.svc/${method}JSON/${process.env.DEV_ID}/${createSignature(method)}/${currentTime()}`;
@@ -16,7 +18,34 @@ const createSession = () => {
   return new Promise((resolve, reject) => {
     rp(url)
       .then((response) => {
-        console.log(response);
+        resolve(JSON.parse(response));
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
+};
+
+const getLeaderboard = (sessionId, queueId, rankingCriteria) => {
+  const method = 'getleaderboard';
+// queueId = 474; 
+// ID	Name	In-game name
+// 474	Solo	LIVE Solo
+// 475	Duo	LIVE Duo
+// 476	Quad	LIVE Quad
+
+// rankingCriteria 
+// ID	Description
+// 1	Team Wins
+// 2	Team Average Placement (shown below)
+// 3	Individual Average Kills
+// 4	Winrate
+  const url = `http://api.realmroyale.com/realmapi.svc/${method}JSON/${process.env.DEV_ID}/${createSignature(method)}/${sessionId}/${currentTime()}/${queueId}/${rankingCriteria}`;
+
+  return new Promise((resolve, reject) => {
+    rp(url)
+      .then((response) => {
         resolve(JSON.parse(response));
       })
       .catch((error) => {
@@ -30,5 +59,6 @@ const createSession = () => {
 module.exports = {
   currentTime,
   createSignature,
-  createSession
+  createSession,
+  getLeaderboard
 };
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-const { createSession } = require('../helper/helper');
+const { createSession, getLeaderboard } = require('../helper/helper');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  const method = 'ping';
-  const url =  `http://api.realmroyale.com/realmapi.svc/${method}JSON`;
-  
   createSession()
-  .then((sesresponsesion) => {
-    res.json(response);
+  .then((response) => {
+    getLeaderboard(response.session_id)
+    .then((leaderboard) => {
+      res.json(leaderboard);
+    });
+  });
+});
+
+//post request to get leaderboard - send queueId & rankingCriteria
+router.post('/leaderboard', function(req, res) {
+  createSession()
+  .then((response) => {
+    getLeaderboard(response.session_id, req.body.queueId, req.body.rankingCriteria)
+    .then((leaderboards) => {
+      res.json(leaderboards);
+    });
   });
 });
 
